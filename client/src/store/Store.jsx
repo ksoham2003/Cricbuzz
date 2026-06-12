c3c9d7b146404e336363bf3866e56c9073e6642b
Krish <khambhatikrish@gmail.com>
2026-06-12 10:01:15 +0530

    feat(store): add Provider in main

--- client/src/store/Store.jsx
+++ client/src/store/Store.jsx
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/auth/state/slice/AuthSlice"
 
 
-let store = configureStore({
+export let store = configureStore({
     reducer: {
         auth: authReducer,
     }
